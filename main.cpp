ccedebd3e58fc029e8f96e4b4f675b34fbde9a42
Alan <alanhaugen@gmail.com>
2023-09-05 17:55:09 +0200

    Made program to do sesession 3

--- main.cpp
+++ main.cpp
@@ -1,51 +1,86 @@
 #include <iostream>
-#include <math.h>
-#include <time.h>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    srand (time(NULL)); // Do this only once (initialise random)
-
-    int guess = rand() % 100; // Make a random number from 0 to 100
-    int attempts = 10;
+    //Define an array of integers, ask the user for the size, and then for them to
+    //fill out with numbers greater than zero. Controls for errors.
     int input;
 
-    cout << "Guess which number I am thinking of (0 - 100): ";
+    cout << "Please specify size of an array \n";
 
-    while (attempts > 0)
+    cin >> input;
+
+    int *arr = new int(input);
+
+    // Print out the values of the array using a for loop
+    for (int i = 0, size = input; i < size; i++)
     {
+        cout << "Set a number greater than 0 (i>0) for array element #" << i << ": ";
+
         cin >> input;
 
-        // Error handling for cin
-        if (cin.fail())
+        if (input < 0)
         {
-            cout << "Please enter numbers only" << endl;
-            std::cin.clear();
-            std::cin.ignore();
-
+            cout << "Please try again with a number greate than 0\n";
+            i--;
             continue;
         }
 
-        attempts--;
+        arr[i] = input;
 
-        if (input < guess)
-        {
-            cout << "Wrong. Too low! You have " << attempts << " attempts left. Try again: ";
-        }
-        else if (input > guess)
-        {
-            cout << "Wrong. Too high! You have " << attempts << " attempts left. Try again: ";
-        }
-        else
-        {
-            cout << endl << "You are right. You win! " << endl;
-            return 0;
-        }
+        cout << "Element " << i << " is now " << arr[i] << "\n";
     }
 
-    cout << endl << "Game over...";
+    // Add a while loop and three different options for the user to do. Use
+    //switch-case to create this menu
+    do
+    {
+        cout << "\n\nPlease enter one of the following commands:\n\n";
+        cout << "[1] Create array\n";
+        cout << "[2] Print array\n";
+        cout << "[3] Change value of X position of array\n";
+        cout << "[4] Exit/Stop\n";
+
+        cin >> input;
+
+        switch(input)
+        {
+        case 1:
+            // Create array
+            break;
+        case 2:
+            // Print array
+            break;
+        case 3:
+            // Change value of X position of array
+            break;
+        case 4:
+            // Exit/Stop
+            break;
+        default:
+            cout << "Please add one of the options.\n";
+        }
+    }
+    while(input != 4);
+
+    // What happens if we try to print an empty array? And if we try to change
+    // the value of a position of an array with no values?
+    int emptyArray[] = {};
+
+    for (int i = 0; i < 100; i++)
+    {
+        cout << emptyArray[i];
+        emptyArray[i] = i;
+    }
+
+    //Create a two-dimensional array (matrix) of 3x3, ask users to introduce
+    //an X or an O. Check when there are 3 X’s or 3 O’s in the same row or
+    //the same column
+    // Extra: Check if it’s diagonal as well.
+
 
     return 0;
 }
