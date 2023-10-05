acdd810d084ca002e850c5796339b81f62faeecc
Alan <alanhaugen@gmail.com>
2023-10-05 20:23:18 +0200

    Added text for comp3

--- source/main.cpp
+++ source/main.cpp
@@ -11,27 +11,25 @@ Principia princ;
  *
  * \section intro_sec Introduction
  *
- * For this Compulsory, you will have to create a program using a
- * Procedural Programming Paradigm approach to implement a calculator
- * that will perform the following operations:
+ * This Compulsory will cover elements such as use of pointers, classes and objects (Object Oriented Programming).
+ * 
+ * \section desc_sec Description
  *
- * - Factorial (use recursion)
- * - Solving 3rd degree polynomial equations (addition, subtraction, multiplication)
- * - Simple math equations (addition, subtraction, multiplication and division).
- *
+ * We want to model the archive system of a computer. This will have two types of elements: Folder and File, which will have different types of properties. Specifically, they will both have a name, size, and date of creation. On the other hand, Folder will also have an array of maximum 10 Files, and an array of maximum 5 Folders. We need operations to create new Files and new Folders, with a random size of maximum 100 MB (just generate a number between 0 and 100 for this). We also want to know the names, sizes, and dates of creation, as well as operations to change the name of the Files and Folders. We finally want to be able to print all the information of a given File or Folder (in the case of Folders, that includes printing all Files within it).
+ * 
  * \section Instructions
  *
- * In order to do this, create a menu system that will allow the user to
- * choose between these three options, as well as offer a choice to terminate
- * the program. Use functions to keep the code as clear and modular as
- * possible. Control for unexpected inputs (strings where numbers are expected).
- * Use XML documentation practices for all defined functions. The submission
- * will be a PDF with Surname_Name_Compulsory2 and have the following:
+ * In the main program, we want to be able to create all of these types of objects, and we also want to look through the Files in a given folder and return the information (name, size, date of creation) of the largest File. For these, the user needs to specify the name of the Folder they want to look through for both of these operations. Create in the main one Folder that will hold all this information.
  *
- * Link to GitHub repository (public!) with all the project files and code
- * Brief paragraph discussing the benefits of using recursion in certain
- * scenarios versus an iterative approach.
- * How you feel you have progressed in programming since the start of the semester.
+ * \section Submission
+ * 
+ * The submission should be in the format Surname_Name_Compulsory3 on Canvas. Clean code and XML comments are strongly encouraged. The file should be a PDF file including the following:
+ * 
+ * Link to the GitHub repository (public!) with the code and project files.
+ * Implementation UML diagrams for the classes you have used
+ * A brief paragraph describing where you feel you are with programming, what do you feel most comfortable doing from what you\'ve learned this year, and what you feel you need to improve on the most from what you've learned this year.
+ *
+ *
 */
 
 /// The main entry point of the program
